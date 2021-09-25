5e95290f62dc1b81e61b970b0e6e1398ffe62850
Ernest Salim <salim.ernest17@gmail.com>
2021-09-25 12:51:54 +0700

    feat: toxicity modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,28 +4,29 @@ import './Modal.scss';
 
 function Modal({ title, description, buttons, visible, figure }) {
     return (
-        visible && (
-            <div className="modal__wrapper">
-                <div className="modal__background" />
-                <div className="modal__container">
-                    <h1 textSize="md" textColor="#000000">
-                        {title}
-                    </h1>
-                    {description !== '' && (
-                        <p textSize="sm" textColor="#000000">
-                            {description}
-                        </p>
-                    )}
-                    <div className="modal__buttons">
-                        {buttons.map((button, index) => (
-                            <button type="button" key={index} onClick={button.onClick}>
-                                {button.text}
-                            </button>
-                        ))}
-                    </div>
+        <div
+            className="modal__wrapper"
+            style={{ opacity: visible ? 1 : 0, pointerEvents: visible ? 'unset' : 'none' }}
+        >
+            <div className="modal__background" />
+            <div className="modal__container">
+                <h1 textSize="md" textColor="#000000">
+                    {title}
+                </h1>
+                {description !== '' && (
+                    <p textSize="sm" textColor="#000000">
+                        {description}
+                    </p>
+                )}
+                <div className="modal__buttons">
+                    {buttons.map((button, index) => (
+                        <button type="button" key={index} onClick={button.onClick}>
+                            {button.text}
+                        </button>
+                    ))}
                 </div>
             </div>
-        )
+        </div>
     );
 }
 
